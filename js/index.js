5de15ac553a2fb0f4b65d0c9107fd301ebaefcd0
Alex <lexshpin@gmail.com>
2020-03-30 17:08:20 +0500

    Finished the game

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
+
 let width = canvas.width;
 let height = canvas.height;
 
@@ -29,12 +30,23 @@ function drawScore() {
 
 // Game Over message
 function gameOver() {
-    clearInterval(invervalId);
+    clearInterval(intervalId);
     ctx.font = '60px Courier';
     ctx.fillStyle = 'black';
     ctx.textAlign = 'center';
     ctx.textBaseLine = 'middle';
     ctx.fillText('Game Over', width / 2, height / 2);
+}
+
+// Draw a circle
+function circle (x, y, radius, fillCircle) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    if (fillCircle) {
+        ctx.fill();
+    } else {
+        ctx.stroke();
+    }
 }
 
 // Creating blocks
@@ -43,7 +55,7 @@ function Block (col, row) {
     this.row = row;
 }
 
-// Draw a square
+// Draw a square in the position of a cell
 Block.prototype.drawSquare = function (color) {
     let x = this.col * blockSize;
     let y = this.row * blockSize;
@@ -51,7 +63,7 @@ Block.prototype.drawSquare = function (color) {
     ctx.fillRect(x, y, blockSize, blockSize);
 }
 
-// Draw a circle
+// Draw a circle in the position of a cell
 Block.prototype.drawCircle = function (color) {
     let centerX = this.col * blockSize + blockSize / 2;
     let centerY = this.row * blockSize + blockSize / 2;
@@ -114,3 +126,85 @@ Snake.prototype.move = function () {
         this.segments.pop();
     }
 }
+
+// Check if Snake hit smth
+Snake.prototype.checkCollision = function (head) {
+    let leftCollision = (head.col === 0);
+    let topCollision = (head.row === 0);
+    let rightCollision = (head.col === widthInBlocks - 1);
+    let bottomCollision = (head.row === heightInBlocks - 1);
+
+    let wallCollision = leftCollision || topCollision || rightCollision || bottomCollision;
+
+    let selfCollision = false;
+
+    for (let i = 0; i < this.segments.length; i++) {
+        if (head.equal(this.segments[i])) {
+            selfCollision = true;
+        }
+    }
+
+    return wallCollision || selfCollision;
+}
+
+// Control the Snake with the keyboard
+let directions = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+}
+
+$('body').keydown(function (event) {
+    let newDirection = directions[event.keyCode];
+    if (newDirection !== undefined) {
+        snake.setDirection(newDirection);
+    }
+})
+
+// Moving the Snake without hitting itself
+Snake.prototype.setDirection = function (newDirection) {
+    if (this.direction === 'up' && newDirection == 'down') {
+        return;
+    } else if (this.direction === 'right' && newDirection === 'left') {
+        return;
+    } else if (this.direction === 'down' && newDirection === 'up') {
+        return;
+    } else if (this.direction === 'left' && newDirection === 'right') {
+        return;
+    }
+
+    this.nextDirection = newDirection;
+}
+
+
+// Creating the Apple
+function Apple () {
+    this.position = new Block(10, 10);
+}
+
+// Draw the Apple
+Apple.prototype.draw = function () {
+    this.position.drawCircle('LimeGreen');
+}
+
+// Move the Apple randomly
+Apple.prototype.move = function () {
+    let randomCol = Math.floor(Math.random() * (widthInBlocks - 2)) + 1;
+    let randomRow = Math.floor(Math.random() * (heightInBlocks - 2)) + 1;
+    this.position = new Block(randomCol, randomRow);
+}
+
+// Create the Snake and the Apple
+let snake = new Snake();
+let apple = new Apple();
+
+// Run the game
+let intervalId = setInterval(function () {
+    ctx.clearRect(0, 0, width, height);
+    drawScore();
+    snake.move();
+    snake.draw();
+    apple.draw();
+    drawBorder();
+}, 100);
